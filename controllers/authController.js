07a08f628c43beb2d9809e7cf851e22e797f28f6
Naveen <vnaveenlgp2001@gmail.com>
2024-11-27 16:04:37 +0530

    login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,13 +95,14 @@ async function googleAuth(req, res) {
   console.log("authController.js")
   try {
     const user = req.user; // Retrieved from Passport's `req.user` after successful authentication
-    console.log(user,"authController.js")
+    console.log(user, "authController.js")
 
 
     // Generate a custom JWT token for the user
-    const token = jwt.sign({ id: user.userId, email: user.email, firstName: user.firstName, lastName: user.lastName, profilePicture: user.profilePicture,type: user.type }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+    const token = jwt.sign({ id: user.userId, email: user.email, firstName: user.firstName, lastName: user.lastName, profilePicture: user.profilePicture, type: user.type }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
     // res.redirect(`http://localhost:3000?token=${token}`);
-    res.redirect(`https://forex-news-naveen4.vercel.app?token=${token}`);
+    res.redirect(`https://forex-news.netlify.app?token=${token}`);
+    // res.redirect(`https://forex-news-naveen4.vercel.app?token=${token}`);
 
   } catch (error) {
     res.status(500).json({ error: "Error during Google authentication" });
